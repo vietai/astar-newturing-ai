1dc7fd77940b98f78f2623d5cb6c3e9f8c19c336
Alex Lee <thanhdungfb@gmail.com>
2021-05-17 21:40:26 +0700

    challences

--- src/containers/Challenges/index.js
+++ src/containers/Challenges/index.js
@@ -1,66 +1,103 @@
 import '../../css/tachyons.min.css';
 import '../../css/styles.css';
+import { challenges } from '../../assets/data.json';
 
 const Challenges = () => {
   return (
-    <div className="dn db-l w-100 bg-pc-red mt5 mb6">
-      <div className="w-70 center pv5">
-        <p className="_title f3 fw6 tc tracked-mega ttu pc-white">
-          Existing Challenges for startups
-        </p>
+    <>
+      {/* desktop */}
+      <div className="dn db-l w-100 bg-pc-red mt5 mb6">
+        <div className="w-70 center pv5">
+          <p className="_title f3 fw6 tc tracked-mega ttu pc-white">
+            {challenges.title}
+          </p>
 
-        <div className="_mansory w-100 flex flex-wrap justify-around mt4 relative">
-          <div className="w-40 ph3">
-            <div className="w-100 pv4">
-              <p className="f4 fw6 pc-white ttu tracked">
-                01 - Limited understanding on How to apply AI in solving busines
-                problem
-              </p>
+          <div className="_mansory w-100 flex flex-wrap justify-around mt4 relative">
+            <div className="w-40 ph3">
+              <div className="w-100 pv4">
+                <p className="f4 fw6 pc-white ttu tracked">
+                  {challenges.col_1.title}
+                </p>
 
-              <p className="pc-pink-white mt4 tracked lh-copy f6 poppins fw2">
-                Blind spot of how AI can and can’t help unlock certain
-                bottlenecks that startups are facing. How AI can be relevant in
-                their business model. Or limited understanding what technology
-                is easiest to start with or easy to use & what the
-                state-of-the-art AI approaches are.
-              </p>
+                <p className="pc-pink-white mt4 tracked lh-copy f6 poppins fw2">
+                  {challenges.col_1.content}
+                </p>
+              </div>
+
+              <div className="w-100 pv4">
+                <p className="f4 fw6 pc-white ttu tracked">
+                  {challenges.col_3.title}
+                </p>
+
+                <p className="pc-pink-white mt4 tracked lh-copy f6 poppins fw2">
+                  {challenges.col_3.content}
+                </p>
+              </div>
             </div>
 
-            <div className="w-100 pv4">
-              <p className="f4 fw6 pc-white ttu tracked">
-                03 - HOW TO BUILD DATA PIPELINE
-              </p>
+            <div className="w-40 ph3">
+              <div className="w-100 pv5">
+                <p className="f4 fw6 pc-white ttu tracked lh-copy">
+                  {challenges.col_2.title}
+                </p>
+                <p className="pc-pink-white mt4 tracked lh-copy f6 poppins fw2">
+                  {challenges.col_2.content}
+                </p>
+              </div>
 
-              <p className="pc-pink-white mt4 tracked lh-copy f6 poppins fw2">
-                Data pipeline architecture could be a tricky task. How far is it
-                from Manual → Semi-automatic → Automatic?
-              </p>
+              <img
+                src="images/challenges.png"
+                alt=""
+                className="absolute"
+                style={{ width: '45%', bottom: '-60%', right: '3%' }}
+              />
             </div>
-          </div>
-
-          <div className="w-40 ph3">
-            <div className="w-100 pv5">
-              <p className="f4 fw6 pc-white ttu tracked lh-copy">
-                02 - HOW TO IMPLEMENT AI INFRASTRUCTURE (MLOPS)
-              </p>
-              <p className="pc-pink-white mt4 tracked lh-copy f6 poppins fw2">
-                AI infrastructure (or MLOPS) - or how to integrate AI prototypes
-                into their business - is one of the most common roadblocks of
-                any technology company in Vietnam. How to access and manage the
-                computing resources to test, train and deploy AI algorithms?
-              </p>
-            </div>
-
-            <img
-              src="images/challenges.png"
-              alt=""
-              className="absolute"
-              style={{ width: '45%', bottom: '-60%', right: '3%' }}
-            />
           </div>
         </div>
       </div>
-    </div>
+
+      {/* mobile */}
+
+      <div class="db dn-l w-100 bg-pc-red pv4 mt4">
+        <p class="_title f3 fw6 tc tracked-mega ttu pc-white">
+          {challenges.title}
+        </p>
+
+        <div class="w-80 center">
+          <p class="f5 fw6 pc-white ttu tracked lh-copy mt4">
+            {challenges.col_1.title}
+          </p>
+
+          <p class="pc-pink-white mt4 tracked lh-copy f6 poppins fw2">
+            {challenges.col_1.content}
+          </p>
+        </div>
+
+        <div class="w-80 center">
+          <p class="f5 fw6 pc-white ttu tracked lh-copy mt4">
+            {challenges.col_2.title}
+          </p>
+
+          <p class="pc-pink-white mt4 tracked lh-copy f6 poppins fw2">
+            {challenges.col_2.content}
+          </p>
+        </div>
+
+        <div class="w-80 center">
+          <p class="f5 fw6 pc-white ttu tracked lh-copy mt4">
+            {challenges.col_3.title}
+          </p>
+
+          <p class="pc-pink-white mt4 tracked lh-copy f6 poppins fw2">
+            {challenges.col_3.content}
+          </p>
+        </div>
+
+        <div class="w-80 center relative pt4 pb6">
+          <img src="images/challenges.png" alt="" class="absolute" />
+        </div>
+      </div>
+    </>
   );
 };
 
