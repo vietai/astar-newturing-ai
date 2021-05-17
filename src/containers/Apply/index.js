0e52a1f1502c19c545d726b158b1e1328b9671a0
Alex Lee <thanhdungfb@gmail.com>
2021-05-18 03:59:11 +0700

    update

--- src/containers/Apply/index.js
+++ src/containers/Apply/index.js
@@ -7,7 +7,7 @@ const Apply = () => {
     <div className="db center w-100 w-60-l bg-pc-red br0 br3-l ph3 ph4-l pv5 pv6-l tc pc-white mt0 mt5-l">
       <p className="f2 ttu tracked fw6">APPLY FOR THE FIRST BATCH</p>
       <p className="f6 tracked fw3 mt4">
-        Want to be come the next gen AI companies? Nominate your company.
+        Want to become the next gen AI companies? Nominate your company.
       </p>
       <div
         className="dib bg-pc-green ph5 pv3 br3 white fw5 f6 pointer dim mt5"
