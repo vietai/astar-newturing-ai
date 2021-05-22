34df0f0a64f5493ef5765dc049e27848d1a83580
Alex Lee <thanhdungfb@gmail.com>
2021-05-23 02:25:43 +0700

    update footer copyright

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -4,15 +4,15 @@ import { register_link as registerLink } from '../assets/data.json';
 
 const Footers = () => (
   <>
-    <div className="w-100 center pt3 pb4 ph4 flex justify-between bg-pc-red pc-white mb4 mb0-l mt3">
+    <div className="w-100 center pt3 pb4 ph4 flex justify-between bg-pc-red pc-white mb4 mb0-l mt3 items-start">
       <img src="images/newturing_ai_white.png" className="pr4 w25" alt="" />
-      <div className="lh-copy h-100 flex flex-column justify-between items-end">
+      <div className="lh-copy  flex flex-column justify-between items-end">
         <div className="">
           <img src="images/instagram.svg" alt="" className="ml4" />
           <img src="images/twitter.svg" alt="" className="ml4" />
           <img src="images/fb.svg" alt="" className="ml4" />
         </div>
-        <p className="copyright dn db-l mt3">Copyright 2010 @ VietAI</p>
+        <p className="copyright mt3 pc-pink-white">Copyright 2021 @ NTI</p>
       </div>
     </div>
     <div
