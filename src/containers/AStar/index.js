a3da3fe63541db9aec2ac5cd42dbce3e8afd730d
Alex Lee <thanhdungfb@gmail.com>
2021-06-03 02:02:14 +0700

    data

--- src/containers/AStar/index.js
+++ src/containers/AStar/index.js
@@ -4,15 +4,24 @@ import '../../css/styles.css';
 const AStar = () => (
   <div className="flex flex-column w-100 pt3 pt5-l tc mt5">
     <p className="w-80 w-60-l center f3 f2-l fw7 pc-red ttu tracked-l mt5-l ph3">
-      WHAT IS A-STAR?
+      WHAT IS A-STAR BOOTCAMP?
     </p>
 
     <p className="w-80 w-50-l center f f5-l fw5 pc-dark-gray  tracked-l mt3 mt4-l ph3 lh-copy">
-      A-Star is the First AI Bootcamp supported by our Global AI Expert Network
-      with a Personalized AI Assessment for Aspiring Startups in Vietnam.
+      A-Star Bootcamp is founded by a group of serial entrepreneurs and a
+      network of global AI experts. Our program is designed to provide startup
+      community with a personalized assessment of how companies and businesses
+      can integrate Artificial Intelligence into their existing business models
+      in order to reach 10-100x transformation of their value proposition and
+      have a long term positive impact in the world. As a community of AI and
+      industry experts, our goal is to help companies find AI application and
+      technology that is most relevant to their startups and provide a practical
+      and rigorous AI training program that allows companies to build their
+      internal AI capacity and expertise that go way beyond the duration of the
+      program.
     </p>
 
-    <div className = "w5 center bb b--black-40 mt5 dn db-l" />
+    <div className="w5 center bb b--black-40 mt5 dn db-l" />
   </div>
 );
 export default AStar;
