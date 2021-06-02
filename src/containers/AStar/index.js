9c61d02244795e4d3eb9e99a33bbc6c054148b7f
Alex Lee <thanhdungfb@gmail.com>
2021-06-03 02:24:16 +0700

    move astar block

--- src/containers/AStar/index.js
+++ src/containers/AStar/index.js
@@ -2,7 +2,7 @@ import '../../css/tachyons.min.css';
 import '../../css/styles.css';
 
 const AStar = () => (
-  <div className="flex flex-column w-100 pt3 pt5-l tc mt5">
+  <div className="flex flex-column w-100 pt3 pt4-l tc mt5">
     <div className="w5 center bb b--black-40 dn db-l" />
     <p className="w-80 w-60-l center f3 f2-l fw7 pc-red ttu tracked-l mt5-l ph3">
       WHAT IS A-STAR BOOTCAMP?
